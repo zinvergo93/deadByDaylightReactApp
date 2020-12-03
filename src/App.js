cacdaefcc998f718927ea9bb40daf69550d52bc5
Zachary Invergo <zinvergo@gmail.com>
2020-12-03 15:46:24 -0600

    tried implementing audio with javascript command in app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import ReactAudioPlayer from "react-audio-player";
 
 import "./styles/main.scss";
 import Survivor from "./survivorRoulette";
@@ -11,20 +10,21 @@ import Home from "./home";
 export default class App extends Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      isLoading= false
+    };
   }
-
+  
+  audioPlayer = () => {
+    const playSong = document.getElementById('audio-player').play()
+      return playSong
+  }
   render() {
     return (
       <div>
         <Router>
           <NavBar />
-          <ReactAudioPlayer
-            src="../public/Dead by Daylight theme.mp3"
-            src="../public/Dead by Daylight theme.ogg"
-            autoPlay="true"
-            loop="true"
-          />
+          {this.audioPlayer()}
           <Switch>
             <Route exact path="/" component={Home} />
             <Route path="/survivor-roulette" component={Survivor} />

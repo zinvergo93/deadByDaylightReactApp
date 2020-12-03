aad9dbf5148bf1356bf8321f82018e0e71cdab50
Zachary Invergo <zinvergo@gmail.com>
2020-12-03 16:40:39 -0600

    hopeful finalization of audio issues

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import ReactAudioPlayer from "react-audio-player";
 
 import "./styles/main.scss";
 import Survivor from "./survivorRoulette";
@@ -18,7 +19,18 @@ export default class App extends Component {
       <div>
         <Router>
           <NavBar />
-
+          <ReactAudioPlayer
+            src="Dead by Daylight theme.mp3"
+            preload="auto"
+            autoPlay
+            loop
+          />
+          <ReactAudioPlayer
+            src="Dead by Daylight theme.ogg"
+            preload="auto"
+            autoPlay
+            loop
+          />
           <Switch>
             <Route exact path="/" component={Home} />
             <Route path="/survivor-roulette" component={Survivor} />
